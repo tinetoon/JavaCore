efc6a1cfdcceaa153aa5526add4521d824231c2e
Дюжаков Антон <tinetoon@mail.ru>
2021-09-19 13:40:56 +0300

    Примеры ч.2

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,5 +1,7 @@
 package Test;
 
+import java.util.Locale;
+
 /**
  * Класс тестирования различных примеров кода
  * Ответственный за создание класса - А.А. Дюжаков
@@ -9,10 +11,38 @@ package Test;
 public class Main {
     public static void main(String[] args) {
 
-        // 1 пример
+//        example01();
+//        example02();
+        example03();
+
+    }
+
+    // 1 пример
+    public static void example01() {
         byte а = 127;
-        а++;
+        а++; // пределы byte от -128 до 127 (с учётом 0)
         System.out.println("Пример 1, ответ: " + а);
+    }
+
+    // 02 пример
+    public static void example02() {
+        String a = "java";
+        a.toUpperCase(); // Не изменяет строку, а только возвращает строку в верхнем регистре
+        System.out.println(a);
+        System.out.println(a.toUpperCase());
+    }
+
+    // 03 пример
+    public static void example03() {
+        String s1 = "Java";
+        String s2 = new String("Java");
+        System.out.print(s1 == s2); // Сравнивает ссылки на объекты - false
+        System.out.print(" ");
+        System.out.print(s1.equals(s2)); // Сравнивает посимвольно - true
+    }
+
+    // 0 пример
+    public static void example0() {
 
     }
 }
