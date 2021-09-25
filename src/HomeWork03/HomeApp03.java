10cc691d9373b9a7d5a3130e948a2e416deff255
Дюжаков Антон <tinetoon@mail.ru>
2021-09-25 14:31:22 +0300

    А.А. Дюжаков - ДЗ № 3 ч.4 (задание № 1)

--- src/HomeWork03/HomeApp03.java
+++ src/HomeWork03/HomeApp03.java
@@ -12,95 +12,20 @@ public class HomeApp03 {
 
     public static void main(String[] args) {
 
-        int[] testIntArr = new int[5]; // Объявляем целочисленный массив из 5 элементов
-        Tools.fillArray(testIntArr,0,20); // Наполняем массив случайными числами
-        System.out.println("1.1 Оригинальный целочисленный массив");
-        Tools.printArray(testIntArr);
-//        testIntArr = swapElements(testIntArr, 2, 4);
-        String[] testStrArr = new String[] {"Каждый", "Охотник", "Желает", "Знать", "Где", "Сидит", "Фазан"}; // Объявляем и инициализируем массив строкового типа
-//        testStrArr[1] = "Фазан";
-//        testStrArr[6] = "Охотник";
+        // Объявляем и инициализируем новый строковый массив
+        NewArray<String> newStringArr2 = new NewArray<>(String.class,
+                "Каждый", "Охотник", "Желает", "Знать", "Где", "Сидит", "Фазан");
 
-        System.out.println("1.2 Оригинальный строковый массив"); // Выводим массив в консоль
-        Tools.printStrArray(testStrArr);
-//        testStrArr = swapElements(testStrArr, 2, 4);
-
-        TestArray<String> genStr = new TestArray<>("Hello"); // Создаём ссылочный массив из строки
-        genStr.showType(); // Выводим в консоль информацию о типе массива
-        System.out.println("Массив genStr[]: " + genStr.getObj()); // Выводим массив в консоль
-
-        TestArray<String[]> newArr = new TestArray<>(testStrArr); // Создаём ссылочный массив из массива строкового типа
-
-        newArr.showType(); // Выводим в консоль информацию о типе массива
-        System.out.println("1.3 Оригинальный ссылочный массив");
-        System.out.println("Длина массива (кол-во элементов): " + newArr.getObj().length);
-        System.out.println("Максимальный номер элемента массива: " + (newArr.getObj().length - 1));
-
-        Tools.printStrArray(newArr.getObj());
-        for (int i = 0; i < newArr.getObj().length; i++) {
-            System.out.println("Элемент массива № " + i + " : " + newArr.getObj()[i]);
-        }
-
-        // операции для метода
-//        String[] temp = new String[2];
-//        temp[0] = testStrArr[1];
-//        temp[1] = testStrArr[6];
-//
-//        testStrArr[1] = temp[1];
-//        testStrArr[6] = temp[0];
-//
-//        System.out.println("1.3 Новый массив");
-//        Tools.printStrArray(testStrArr);
-
-        // операции для ссылочного метода
-//        String[] temp = new String[2];
-//        temp[0] = newArr.getObj()[1];
-//        temp[1] = newArr.getObj()[6];
-//        newArr.setObj(newArr.swapElementsNew(testStrArr, 1, 6));
-        newArr.setObj(newArr.swapElementsNew(newArr.getObj(), 1, 6));
-//        TestArray<String[]> newTempArr = new TestArray<>(temp);
-
-        // TestArray<String[]> newArr = new TestArray<String[]>(testStrArr)
-//        newArr = swapElements(newArr, 1, 6);
-        System.out.println("1.4 Новый ссылочный массив");
-        Tools.printStrArray(newArr.getObj());
-
-        char[] testCharArr = new char[] {'~', '@', '#', '$', '%', '&', '*'}; // Объявляем и инициализируем массив символьного типа
-        TestArray<char[]> newCharArr = new TestArray<>(testCharArr); // Создаём ссылочный массив из массива символьного типа
-        System.out.println("1.5 Оригинальный ссылочный массив");
-        Tools.printCharArray(newCharArr.getObj());
-        newCharArr.setObj(newCharArr.swapElementsChar(newCharArr.getObj(), 1, 6));
-        System.out.println("1.6 Новый ссылочный массив");
-        Tools.printCharArray(newCharArr.getObj());
-
-//        String[] testStrArr2 = new String[] {"Каждый", "Охотник", "Желает", "Знать", "Где", "Сидит", "Фазан"}; // Объявляем и инициализируем массив строкового типа
-//        Array<String> newStringArr = new Array<>("Каждый", "Охотник", "Желает", "Знать", "Где", "Сидит", "Фазан"); // Создаём ссылочный массив из массива строкового типа
-        NewArray<String> newStringArr2 = new NewArray<>(String.class,"Каждый", "Охотник", "Желает", "Знать", "Где", "Сидит", "Фазан");
-        System.out.println("1.7 Оригинальный ссылочный массив");
+        // Печатаем информацию о массиве в консоль
+        System.out.println("1.1 Оригинальный ссылочный массив");
         newStringArr2.printArray();
+
+        // Вызываем метод замены элементов (меняем местами первый и шестой элементы)
         newStringArr2.swapElements(newStringArr2.getNewArray(), 1, 6);
-        System.out.println("1.8 Новый ссылочный массив");
-        newStringArr2.printArray();
 
-//        NewArray<char> newCharArr2 = new NewArray<>(char.class,'~', '@', '#', '$', '%', '&', '*');
-//        Tools.printCharArray(newCharArr2.getNewArray());
-//        NewArray<char> newCharArr2 = new NewArray<>(char.class, '@');
+        // Проверяем работу метода повторным выводом в консоль информации о массиве
+        System.out.println("1.1 Новый ссылочный массив");
+        newStringArr2.printArray();
     }
 
-    //Метод, который меняет два элемента массива местами
-//    public static TestArray[] swapElements(TestArray[] arr, int firstElementNum, int secondElementNum) {
-//        TestArray[] temp = new TestArray[2];
-//        TestArray[] tempArr = new TestArray[arr.length];
-//        temp[1] = arr[firstElementNum];
-//        temp[2] = arr[secondElementNum];
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            tempArr[i] = arr[i];
-//        }
-//
-//        arr[firstElementNum] = temp[2];
-//        arr[secondElementNum] = temp[1];
-//
-//        return arr;
-//    }
 }
