b46234da4716a7f94e18daadbc5d8a4d62e5074f
Дюжаков Антон <tinetoon@mail.ru>
2021-09-25 11:45:11 +0300

    А.А. Дюжаков - ДЗ № 3 ч.2

--- src/HomeWork03/HomeApp03.java
+++ src/HomeWork03/HomeApp03.java
@@ -14,27 +14,34 @@ public class HomeApp03 {
 
     public static void main(String[] args) {
 
-        int[] testIntArr = new int[5]; // Объявляем массив из 5 элементов
-        Tools.fillArray(testIntArr,0,20);
-        System.out.println("1.1 Оригинальный массив");
+        int[] testIntArr = new int[5]; // Объявляем целочисленный массив из 5 элементов
+        Tools.fillArray(testIntArr,0,20); // Наполняем массив случайными числами
+        System.out.println("1.1 Оригинальный целочисленный массив");
         Tools.printArray(testIntArr);
 //        testIntArr = swapElements(testIntArr, 2, 4);
-        String[] testStrArr = new String[] {"Каждый", "Охотник", "Желает", "Знать", "Где", "Сидит", "Фазан"};
+        String[] testStrArr = new String[] {"Каждый", "Охотник", "Желает", "Знать", "Где", "Сидит", "Фазан"}; // Объявляем и инициализируем массив строкового типа
 //        testStrArr[1] = "Фазан";
 //        testStrArr[6] = "Охотник";
 
-        System.out.println("1.2 Оригинальный массив");
+        System.out.println("1.2 Оригинальный строковый массив"); // Выводим массив в консоль
         Tools.printStrArray(testStrArr);
 //        testStrArr = swapElements(testStrArr, 2, 4);
 
-        TestArray<String> genStr = new TestArray<>("Hello");
-        genStr.showType();
-        System.out.println("genStr.getObject(): " + genStr.getObj());
+        TestArray<String> genStr = new TestArray<>("Hello"); // Создаём ссылочный массив из строки
+        genStr.showType(); // Выводим в консоль информацию о типе массива
+        System.out.println("Массив genStr[]: " + genStr.getObj()); // Выводим массив в консоль
 
-        TestArray<String[]> newArr = new TestArray<>(testStrArr);
-        newArr.showType();
-        System.out.println("1.3 Оригинальный массив");
+        TestArray<String[]> newArr = new TestArray<>(testStrArr); // Создаём ссылочный массив из массива строкового типа
+
+        newArr.showType(); // Выводим в консоль информацию о типе массива
+        System.out.println("1.3 Оригинальный ссылочный массив");
+        System.out.println("Длина массива (кол-во элементов): " + newArr.getObj().length);
+        System.out.println("Максимальный номер элемента массива: " + (newArr.getObj().length - 1));
+
         Tools.printStrArray(newArr.getObj());
+        for (int i = 0; i < newArr.getObj().length; i++) {
+            System.out.println("Элемент массива № " + i + " : " + newArr.getObj()[i]);
+        }
 
         // операции для метода
 //        String[] temp = new String[2];
@@ -51,22 +58,32 @@ public class HomeApp03 {
         String[] temp = new String[2];
         temp[0] = newArr.getObj()[1];
         temp[1] = newArr.getObj()[6];
+//        newArr.setObj(newArr.swapElementsNew(testStrArr, 1, 6));
+        newArr.setObj(newArr.swapElementsNew(newArr.getObj(), 1, 6));
 
-        TestArray<String[]> newTempArr = new TestArray<>(temp);
 
-        System.out.println("1.3 Новый массив");
-        Tools.printStrArray(testStrArr);
+//        TestArray<String[]> newTempArr = new TestArray<>(temp);
+
+        // TestArray<String[]> newArr = new TestArray<String[]>(testStrArr)
+//        newArr = swapElements(newArr, 1, 6);
+        System.out.println("1.4 Новый ссылочный массив");
+        Tools.printStrArray(newArr.getObj());
     }
 
     //Метод, который меняет два элемента массива местами
-    public static TestArray[] swapElements(TestArray[] arr, int firstElementNum, int secondElementNum) {
-        TestArray[] temp = new TestArray[2];
-        temp[1] = arr[firstElementNum];
-        temp[2] = arr[secondElementNum];
-
-        arr[firstElementNum] = temp[2];
-        arr[secondElementNum] = temp[1];
-
-        return arr;
-    }
+//    public static TestArray[] swapElements(TestArray[] arr, int firstElementNum, int secondElementNum) {
+//        TestArray[] temp = new TestArray[2];
+//        TestArray[] tempArr = new TestArray[arr.length];
+//        temp[1] = arr[firstElementNum];
+//        temp[2] = arr[secondElementNum];
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            tempArr[i] = arr[i];
+//        }
+//
+//        arr[firstElementNum] = temp[2];
+//        arr[secondElementNum] = temp[1];
+//
+//        return arr;
+//    }
 }
--- src/HomeWork03/TestArray.java
+++ src/HomeWork03/TestArray.java
@@ -1,23 +1,48 @@
 package HomeWork03;
 
+import java.lang.reflect.Array;
+
+/**
+ * Класс параметризации объекта
+ * Ответственный за создание класса - А.А. Дюжаков
+ * Дата создания: 23.09.2021
+ */
+
 public class TestArray<T> {
 
     private T obj;
 
+    // Конструктор класса
     public TestArray(T оbj) {
         this.obj = оbj;
     }
+
+    // Метод вывода информации о типе массива
+    public void showType() {
+        System.out.println("Тип T: " + obj.getClass().getName());
+    }
+
+    // Метод замены элемента массива
+    public String[] swapElementsNew(String[] arr, int firstElementNum, int secondElementNum) {
+        String[] temp = new String[2];
+
+        temp[0] = arr[firstElementNum];
+        temp[1] = arr[secondElementNum];
+
+        arr[firstElementNum] = temp[1];
+        arr[secondElementNum] = temp[0];
+
+        return arr;
+    }
+
+
 
     public T getObj() {
         return obj;
     }
 
-//    public void setObj(T obj) {
-//        this.obj = obj;
-//    }
-
-    public void showType() {
-        System.out.println("Тип T: " + obj.getClass().getName());
+    public void setObj(T obj) {
+        this.obj = obj;
     }
 
 }
