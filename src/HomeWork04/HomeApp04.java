9903658716aa36a814346918d844dc4e2b5dd973
Дюжаков Антон <tinetoon@mail.ru>
2021-10-02 10:54:07 +0300

    А.А. Дюжаков - ДЗ № 4 ч.2

--- src/HomeWork04/HomeApp04.java
+++ src/HomeWork04/HomeApp04.java
@@ -19,30 +19,49 @@ public class HomeApp04 {
 //                "Quisque", "mollis", "suscipit", "felis", "hendrerit", "auctor.",
 //                "Quisque", "ac", "mi", "viverra", "pretium", "tortor"};
 
+        System.out.println("===== Задание № 1 =====");
+
         String[] Lorem = new String[] {"Сегодня", "отличный", "день", "я", "пошёл", "сегодня", "домой", "после",
                 "работы", "дома", "меня", "ждал", "кот", "я", "люблю", "своего", "кота",
                 "и", "сегодня", "у", "моего", "кота", "тоже", "отличный", "день"};
 
         Set<String> uniqWord = new HashSet<>(Arrays.asList(Lorem));
-        System.out.println("Всего слов: " + Lorem.length);
+        for (int i = 0; i < Lorem.length; i++) {
+            System.out.print(Lorem[i] + " ");
+        }
+
+        // Выводим на печать количество слов в массиве
+        System.out.println("\nВсего слов: " + Lorem.length);
+
+        // Подсчитываем уникальные слова и выводим их на печать
         System.out.println("Количество уникальных слов: " + uniqWord.size());
         System.out.println(uniqWord);
 
-        Map<String, Integer> letters = collection(Lorem);
+        // Подсчитываем и выводим в консоль количество повторений слов
+        Map<String, Integer> letters = wordCount(Lorem);
         printCollection(letters);
 
+        System.out.println("===== Задание № 2 =====");
+
+        // Создаём новую телефонную книгу
         PhoneBook directory = new PhoneBook();
         directory.add("Антон", "+79991234567(1)");
         directory.add("Сергей", "+79991234567");
         directory.add("Борис", "+79991234567");
         directory.add("Антон", "+79991234567(2)");
-        System.out.println("Сергей, тел.: " + directory.get("Сергей"));
-        System.out.println("Антон, тел.: " + directory.get("Антон"));
+//        System.out.println("Сергей, тел.: " + directory.get("Сергей"));
+//        System.out.println("Антон, тел.: " + directory.get("Антон"));
+
+        get2(directory, "Антон");
+        get2(directory, "Иван");
+
+        PhoneBook directory2 = new PhoneBook();
+        get2(directory2, "Иван");
 
     }
 
     // Метод подсчёта уникальных слов
-    public static Map<String, Integer> collection (String[] arr) {
+    public static Map<String, Integer> wordCount (String[] arr) {
         Map<String, Integer> collection = new HashMap<String, Integer>();
 
         for (int i = 0; i < Arrays.asList(arr).size(); i++) {
@@ -62,6 +81,60 @@ public class HomeApp04 {
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             System.out.println("Слово /" + entry.getKey() + "/\t повторяется " + entry.getValue() + " раз");
         }
+    }
+
+    // Метод поиска телефона
+    public static PhoneCard get(PhoneBook book, String name) {
+//        return hashMap.get(name);
+
+        PhoneCard tempCard = new PhoneCard(); // Объявляем временную карточку
+
+        tempCard.add(name, "Номер неизвестен"); // Значение по умолчанию
+
+        if (!book.getPhoneBook().isEmpty()) { // Проверяем пустоту телефонной книги
+
+            for (PhoneCard card : book.getPhoneBook()) { // Бежим по всем элементам телефонной книги
+//                System.out.println(card.containsKey(name));
+                if (card.getHashCard().containsKey(name)) { // Проверяем наличие ключа с именем
+                    tempCard.add(name, card.getHashCard().get(name)); // Если всё ОК, присваиваем номер во временную карточку и возвращаем её
+                    return card;
+                } else {
+                    return tempCard;
+                }
+            }
+        } else {
+            System.out.println("Телефонная книга пуста");
+        }
+        return tempCard;
+    }
+
+    // Метод поиска телефона
+    public static void get2(PhoneBook book, String name) {
+//        return hashMap.get(name);
+
+        PhoneCard tempCard = new PhoneCard(); // Объявляем временную карточку
+
+        tempCard.add(name, "Номер неизвестен"); // Значение по умолчанию
+        int tmpGood = 0;
+
+        if (!book.getPhoneBook().isEmpty()) { // Проверяем пустоту телефонной книги
+
+            for (PhoneCard card : book.getPhoneBook()) { // Бежим по всем элементам телефонной книги
+//                System.out.println(card.containsKey(name));
+                if (card.getHashCard().containsKey(name)) { // Проверяем наличие ключа с именем
+//                    tempCard.add(name, card.getHashCard().get(name)); // Если всё ОК, присваиваем номер во временную карточку и возвращаем её
+                    card.printCard();
+                    tmpGood = 1;
+                }
+            }
+
+            if (tmpGood == 0) {
+                    tempCard.printCard();
+            }
+        } else {
+            System.out.println("Телефонная книга пуста");
+        }
+//        tempCard.printCard();
     }
 
 }
--- src/HomeWork04/PhoneBook.java
+++ src/HomeWork04/PhoneBook.java
@@ -18,27 +18,47 @@ import java.util.Map;
 
 public class PhoneBook {
 
-    private ArrayList<String> phoneBook;
-    private HashMap<String, String> hashMap;
+    private ArrayList<PhoneCard> phoneBook;
+//    private HashMap<String, String> hashMap;
 
     public PhoneBook() {
         this.phoneBook = new ArrayList<>();
-        this.hashMap = new HashMap<>();
+//        this.hashMap = new HashMap<>();
+
     }
 
     // Метод добавления записей в телефонную книгу
     public void add(String name, String phone) {
-        hashMap.put(name, phone);
+//        hashMap.put(name, phone);
+        PhoneCard field = new PhoneCard();
+        field.add(name, phone);
+        phoneBook.add(field);
     }
 
-    // Метод поиска телефона
-    public String get(String name) {
-        return hashMap.get(name);
-    }
+//    // Метод поиска телефона
+//    public PhoneCard get(String name) {
+////        return hashMap.get(name);
+//
+//        PhoneCard tempCard = new PhoneCard();
+//        tempCard.add(name, "Номер неизвестен");
+//        if (!phoneBook.isEmpty()) {
+//            for (PhoneCard : phoneBook.) {
+//
+//            }
+//        } else {
+//            System.out.println("Телефонная книга пуста");
+//        }
+//        return tempCard;
+//    }
 
-    public void get2(String name) {
-        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
-            System.out.println("Пользователь: " + entry.getKey() + ", тел.: " + entry.getValue() + " раз");
-        }
+//    public void get2(String name) {
+//        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+//            System.out.println("Пользователь: " + entry.getKey() + ", тел.: " + entry.getValue() + " раз");
+//        }
+//    }
+
+
+    public ArrayList<PhoneCard> getPhoneBook() {
+        return phoneBook;
     }
 }
